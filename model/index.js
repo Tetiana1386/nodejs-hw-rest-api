67ca7638f84eb78bf3199e12b30d8b31192e34fc
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-25 23:41:16 +0300

    adds remove, update

--- model/index.js
+++ model/index.js
@@ -12,15 +12,39 @@ const listContacts = async () => {
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts()
-  const contact = contacts.find(contact => contact.id === contactId)
+  const contact = contacts.find(contact => contact.id.toString() === contactId)
   return contact
 }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  const contacts = await listContacts()
+  const contact = contacts.find(contact => contact.id.toString() === contactId)
+  if (!contact) return
+  const newContacts = contacts.filter(contact => contact.id.toString() !== contactId)
+  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2))
+  return contact
+}
 
-const addContact = async (body) => {}
+const addContact = async (body) => {
+  const contacts = await listContacts()
+  const newContact = { id: shortid.generate(), ...body }
+  const newContacts = [...contacts, newContact]
+  await fs.writeFile(
+    contactsPath,
+    JSON.stringify(newContacts, null, 2),
+    'utf-8',
+  )
+  return newContact
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const contacts = await listContacts()
+  const index = contacts.findIndex(({ id }) => id.toString() === contactId)
+  if (index === -1) return
+  contacts[index] = { ...contacts[index], ...body }
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8')
+  return contacts[index]
+}
 
 module.exports = {
   listContacts,
