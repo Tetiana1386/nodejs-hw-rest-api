464f8ee76427c0d60ce5c47273a304fb3fa02e79
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-30 22:19:48 +0300

    adds db

--- model/index.js
+++ model/index.js
@@ -1,48 +1,43 @@
-const fs = require('fs/promises');
-const path = require('path');
 const shortid = require('shortid');
-
-const contactsPath = path.join(__dirname, 'contacts.json');
+const DB = require('./ db');
+const db = new DB(contacts.json);
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, 'utf-8');
-  return JSON.parse(data);
+  return await db.read();
 };
 
 const getContactById = async (contactId) => {
-  const contacts = await listContacts();
+  const contacts = await db.read();
   const contact = contacts.find(contact => contact.id.toString() === contactId);
   return contact;
 };
 
 const removeContact = async (contactId) => {
-  const contacts = await listContacts();
+  const contacts = await db.read();
   const contact = contacts.find(contact => contact.id.toString() === contactId);
   if (!contact) return;
   const newContacts = contacts.filter(contact => contact.id.toString() !== contactId);
-  await fs.writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+  await db.write(newContacts);
   return contact;
 };
 
 const addContact = async (body) => {
-  const contacts = await listContacts();
+  const contacts = await db.read();
   const newContact = { id: shortid.generate(), ...body };
   const newContacts = [...contacts, newContact];
-  await fs.writeFile(
-    contactsPath,
-    JSON.stringify(newContacts, null, 2),
-    'utf-8',
-  );
+  await db.write(newContacts);
   return newContact;
 };
 
 const updateContact = async (contactId, body) => {
-  const contacts = await listContacts();
+  const contacts = await db.read();
   const index = contacts.findIndex((contact) => contact.id.toString() === contactId);
-  if (index === -1) return;
-  contacts[index] = { ...contacts[index], ...body };
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), 'utf-8');
-  return contacts[index];
+  if (index !== -1) {
+    contacts[index] = { ...contacts[index], ...body };
+    await db.write(contacts);
+    return contacts[index];
+  }
+  return null;
 };
 
 module.exports = {
