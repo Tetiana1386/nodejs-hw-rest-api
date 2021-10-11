9a5fcb976af25c196617c8db752853df42d97c52
Tetiana1386 <tanyashostak13@gmail.com>
2021-10-12 00:41:55 +0300

    adds userSchema

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const { HttpCode } = require('./config/constants');
 
 const contactsRouter = require('./routes/api/contacts');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/users', usersRouter);
 app.use('/api/contacts', contactsRouter);
 
 app.use((req, res) => {
